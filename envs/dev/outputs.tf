ff31ca9f600e6d01fb51a48e195b526fa34e1c32
유하준 <hajuny129@gmail.com>
2025-09-09 23:44:59 +0900

    refactor: remove unused tf file

--- envs/dev/outputs.tf
+++ envs/dev/outputs.tf
@@ -4,41 +4,17 @@ output "network_project_id" {
   value       = module.network_project.project_id
 }
 
+output "network_project_number" {
+  description = "Network project number"
+  value       = module.network_project.project_number
+}
+
 output "infrastructure_project_id" {
   description = "Infrastructure project ID"
   value       = module.infrastructure_project.project_id
 }
 
-# Network outputs
-output "vpc_network_name" {
-  description = "VPC network name"
-  value       = module.vpc_network.network_name
-}
-
-output "subnets" {
-  description = "Created subnets"
-  value       = module.vpc_network.subnets
-}
-
-# GKE outputs
-output "gke_cluster_name" {
-  description = "GKE cluster name"
-  value       = module.gke_cluster.cluster_name
-}
-
-output "gke_cluster_endpoint" {
-  description = "GKE cluster endpoint"
-  value       = module.gke_cluster.cluster_endpoint
-  sensitive   = true
-}
-
-# Legacy outputs
-output "legacy_storage_bucket_names" {
-  description = "Names of the created storage buckets (legacy)"
-  value       = module.legacy_storage.bucket_names
-}
-
-output "legacy_compute_instance_names" {
-  description = "Names of the created compute instances (legacy)"
-  value       = module.legacy_compute.instance_names
+output "infrastructure_project_number" {
+  description = "Infrastructure project number"
+  value       = module.infrastructure_project.project_number
 }
--- envs/dev/variables.tf
+++ envs/dev/variables.tf
@@ -4,60 +4,9 @@ variable "billing_account_id" {
   description = "Billing account ID (format: 0123AB-4567CD-8901EF)"
 }
 
-# Legacy variables (for existing resources)
-variable "project_id" {
-  type        = string
-  description = "The existing GCP project ID for legacy resources"
-}
-
-variable "region" {
-  type        = string
-  description = "The GCP region"
-  default     = "asia-northeast3" # Seoul region
-}
-
-variable "zone" {
-  type        = string
-  description = "The GCP zone"
-  default     = "asia-northeast3-a" # Seoul zone
-}
-
+# Environment configuration
 variable "environment" {
   type        = string
   description = "Environment name"
   default     = "dev"
-}
-
-# Legacy storage variables
-variable "bucket_names" {
-  type        = list(string)
-  description = "List of bucket names to create"
-  default     = []
-}
-
-variable "storage_class" {
-  type        = string
-  description = "Storage class for buckets"
-  default     = "STANDARD"
-}
-
-# Legacy compute variables
-variable "instances" {
-  type = map(object({
-    name                   = string
-    machine_type           = string
-    zone                   = string
-    image                  = optional(string, "debian-cloud/debian-12")
-    disk_size              = optional(number, 10)
-    network                = optional(string, "default")
-    subnetwork             = optional(string, null)
-    enable_public_ip       = optional(bool, true)
-    tags                   = optional(list(string), [])
-    labels                 = optional(map(string), {})
-    metadata               = optional(map(string), {})
-    service_account_email  = optional(string, "default")
-    service_account_scopes = optional(list(string), ["cloud-platform"])
-  }))
-  description = "Map of compute instances to create"
-  default     = {}
 }
